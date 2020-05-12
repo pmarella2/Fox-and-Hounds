6c723f104f28e359232cce336f60fb94856a1cf9
Praneeth Babu Marella <9484118+pmarella2@users.noreply.github.com>
2020-05-11 23:13:43 -0400

    [Add] Password reset email backend

--- fox_and_hounds/settings.py
+++ fox_and_hounds/settings.py
@@ -5,7 +5,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 sentry_sdk.init(
     dsn="https://4d268db239ec4e1cb0c5f9f84d799053@o374711.ingest.sentry.io/5236883",
     integrations=[DjangoIntegration()],
-    send_default_pii=True
+    send_default_pii=True,
 )
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django_otp.middleware.OTPMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "csp.middleware.CSPMiddleware",
 ]
 
 ROOT_URLCONF = "fox_and_hounds.urls"
@@ -71,6 +72,13 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "asgiref.inmemory.ChannelLayer",
+        "ROUTING": "fox_and_hounds.routing.channel_routing",
+    },
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -79,6 +87,40 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
+
+OTP_TOTP_ISSUER = "Fox and Hounds"
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("GMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_PASS")
+
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+#SECURE_HSTS_SECONDS = 31556952
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_HSTS_PRELOAD = True
+#SECURE_SSL_REDIRECT = True
+#SECURE_REFERRER_POLICY = "same-origin"
+#SECURE_BROWSER_XSS_FILTER = True
+#SECURE_CONTENT_TYPE_NOSNIFF = True
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_STYLE_SRC = (
+    "'self'",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
+)
+CSP_SCRIPT_SRC = (
+    "'self'",
+    "https://code.jquery.com/jquery-3.5.0.slim.min.js",
+    "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js",
+)
+CSP_IMG_SRC = ("'self'",)
+CSP_FONT_SRC = ("'self'",)
 
 LOGGING = {
     "version": 1,
@@ -125,19 +167,10 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgiref.inmemory.ChannelLayer",
-        "ROUTING": "fox_and_hounds.routing.channel_routing",
-    },
-}
-
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "game-home"
 LOGIN_URL = "login"
-
-OTP_TOTP_ISSUER = "Fox and Hounds"
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
